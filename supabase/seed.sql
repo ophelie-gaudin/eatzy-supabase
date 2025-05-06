65b8ef4b8084dd8401a570a26a1742c82d52da3f
Ophélie Gaudin <ophelie.gaudin@outlook.fr>
2025-05-06 21:25:17 +0200

    Initial commit with Supabase setup

---
+++ supabase/seed.sql
@@ -0,0 +1,7 @@
+-- Example seed data
+-- Uncomment and modify as needed
+
+-- INSERT INTO public.users (id, email)
+-- VALUES 
+--   ('d0fb8273-78c9-4382-8a9f-2d06b148d81e', 'user1@example.com'),
+--   ('8d48c6a7-0935-4517-9067-7d25eb22e5df', 'user2@example.com'); 
